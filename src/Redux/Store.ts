82c31a1c0b5532b13ba4f6fe785359432e47e5a0
YaroshUlad <yarosh.uladzimir@gmail.com>
2022-05-20 12:58:28 +0300

    del comment from store

--- src/Redux/Store.ts
+++ src/Redux/Store.ts
@@ -50,11 +50,3 @@ export let store = {
         this._rerenderEntireTree()
     }
 }
-
-/*export let state = {
-    posts: ['first post', 'Hello im here', 'whats up?', 'im lucky', 'Privet'],
-    dialogsPage: {
-        dialogs: ['Andrew', 'Ivan', 'Oleg', 'Maria', 'Katya'],
-        messages: ['Hello', 'How are you?', 'Im fine', 'its cool']
-    }
-}*/
